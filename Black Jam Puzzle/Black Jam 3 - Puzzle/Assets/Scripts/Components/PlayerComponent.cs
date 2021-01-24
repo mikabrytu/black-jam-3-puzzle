85bda171246c003b9a43fea5260fa8c55782a836
Mikabrytu <mikabrytu@gmail.com>
2021-01-24 14:41:50 -0300

    Make player a picker

--- Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Components/PlayerComponent.cs
+++ Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Components/PlayerComponent.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 
+using Mikabrytu.BJ3.Events;
 using Mikabrytu.BJ3.Systems;
 
 namespace Mikabrytu.BJ3.Components
 {
-    public class PlayerComponent : MonoBehaviour
+    public class PlayerComponent : MonoBehaviour, IPicker
     {
+        [SerializeField] private Transform _picker;
         [SerializeField] private float _speed = 10f;
 
         private MoveSystem moveSystem;
@@ -17,7 +19,25 @@ namespace Mikabrytu.BJ3.Components
 
         private void Update()
         {
-            moveSystem.Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")));
+            float h = Input.GetAxisRaw("Horizontal");
+
+            if (h != 0)
+            {
+                Vector2 pickerPosition = _picker.localPosition;
+                pickerPosition.x = h;
+                _picker.localPosition = pickerPosition;
+            }
+
+            moveSystem.Move(new Vector2(h, Input.GetAxis("Vertical")));
+
+            // TODO: Remove this DEBUG
+            if (Input.GetMouseButtonDown(0))
+                EventManager.Raise(new OnPickableDropEvent());
+        }
+
+        public Vector3 GetPosition()
+        {
+            return _picker.position;
         }
     }
 }
