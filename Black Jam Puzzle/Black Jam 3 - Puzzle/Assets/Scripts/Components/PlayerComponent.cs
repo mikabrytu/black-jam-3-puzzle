e03df484fd2b6a032ec62847f031abe457059431
Mikabrytu <mikabrytu@gmail.com>
2021-01-24 13:08:09 -0300

    Add move system and get input for player

---
+++ Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Components/PlayerComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+using Mikabrytu.BJ3.Systems;
+
+namespace Mikabrytu.BJ3.Components
+{
+    public class PlayerComponent : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 10f;
+
+        private MoveSystem moveSystem;
+
+        private void Start()
+        {
+            moveSystem = new MoveSystem(transform, _speed);
+        }
+
+        private void Update()
+        {
+            moveSystem.Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")));
+        }
+    }
+}
---
+++ Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Systems/MoveSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.BJ3.Systems
+{
+    public class MoveSystem
+    {
+        private Transform target;
+        private float speed;
+
+        public MoveSystem(Transform target, float speed)
+        {
+            this.target = target;
+            this.speed = speed;
+        }
+
+        public void Move(Vector3 vector)
+        {
+            target.Translate(vector * speed * Time.deltaTime);
+        }
+
+        #region Debug
+
+        public void UpdateSpeed(float speed)
+        {
+            this.speed = speed;
+        }
+
+        #endregion
+    }
+}
