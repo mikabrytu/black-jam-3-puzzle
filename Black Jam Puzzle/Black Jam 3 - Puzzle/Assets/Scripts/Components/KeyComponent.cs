9f62556f4267d7177bdd14b541d5e381af4e3cbe
Mikabrytu <mikabrytu@gmail.com>
2021-01-24 14:41:29 -0300

    Add a template component to test the PickSystem

---
+++ Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Components/KeyComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mikabrytu.BJ3.Events;
+using Mikabrytu.BJ3.Systems;
+using System;
+
+namespace Mikabrytu.BJ3.Components
+{
+    public class KeyComponent : MonoBehaviour
+    {
+        [SerializeField] private string[] _allowedTags;
+
+        private PickSystem pickSystem;
+
+        private void Start()
+        {
+            pickSystem = new PickSystem(transform, GetComponent<Collider2D>(), _allowedTags);
+
+            EventManager.AddListener<OnPickableDropEvent>(OnDrop);
+        }
+
+        private void Update()
+        {
+            pickSystem.Follow();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            pickSystem.Register(collision);
+        }
+
+        private void OnDrop(OnPickableDropEvent e)
+        {
+            pickSystem.Unregister();
+        }
+    }
+}
