28a7925746633e14537f3a03eb9cb26a91f05a00
Mikabrytu <mikabrytu@gmail.com>
2021-01-25 11:20:40 -0300

    Add map component to test the procedural system

---
+++ Black Jam Puzzle/Black Jam 3 - Puzzle/Assets/Scripts/Components/MapComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+using Mikabrytu.BJ3.Systems;
+
+namespace Mikabrytu.BJ3.Components
+{
+    public class MapComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _rooms;
+        [SerializeField] private Transform[] _positions;
+        [SerializeField] private int[] _ids;
+        [SerializeField] private int _matrixScale;
+
+        private MapSystem mapSystem;
+
+        private void Start()
+        {
+            mapSystem = new MapSystem(_rooms, _positions, _ids, _matrixScale);
+        }
+
+        public void GenerateMap()
+        {
+            mapSystem.Shuffle();
+        }
+    }
+}
